e1a3e33bb468c2a49381b957429536cb88c71397
SYBRIN\JDeCarvalho <Jean.DeCarvalho@sybrin.com>
2018-02-12 11:06:57 +0200

    Changed login to return a user or null instead of a boolean. Added a register function. Added the ability to add items in the ItemRepo.

--- Penny/Penny/Penny/Models/Item.cs
+++ Penny/Penny/Penny/Models/Item.cs
@@ -14,5 +14,7 @@ namespace Penny.Models
         public Guid SellerId { get; set; }
         public string Condition { get; set; }
         public string Description { get; set; }
+
+        public string image { get; set; }
     }
 }
--- Penny/Penny/Penny/Repositories/ItemRepo.cs
+++ Penny/Penny/Penny/Repositories/ItemRepo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Penny.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,39 @@ namespace Penny.Repositories
     public class ItemRepo
     {
 
-        // Returns a list of all the items
-        //public async Task<IList<ItemRepo>>
+        public async Task<bool> AddItem(string name, string category, double price, string city, Guid sellerId, string condition, string description, string image)
+        {
+            if (!string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(category) && !string.IsNullOrEmpty(city) && !string.IsNullOrEmpty(condition) && !string.IsNullOrEmpty(description) && !string.IsNullOrEmpty(image))
+            {
+                Item item = new Item()
+                {
+                    Id = new Guid(),
+                    Name = name,
+                    Category = category,
+                    Price = price,
+                    City = city,
+                    SellerId = sellerId,
+                    Condition =condition,
+                    Description = description,
+                    image = image
+
+                };
+
+                try
+                {
+                    await ConnectionManager.Insert<Item>(item);
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
